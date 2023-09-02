afac23e4e0415fc000ece6c82c431423a220e8b8
Daniel <jintannaegi@gmail.com>
2023-09-02 12:33:05 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,tsx}',
+    './components/**/*.{html,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
