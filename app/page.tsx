c4050a05d7a17040d4e2292b02167e468a9e7233
Daniel <jintannaegi@gmail.com>
2023-09-05 12:14:45 +0000

    move the codes into the login component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
 import Link from "next/link"
+import Login from "./login/page"
 
 export default function Home() {
   return (
-    <main className="h-screen bg-gray-200 w-full flex justify-center items-center">
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h1 className="card-title text-3xl font-bold">Login</h1>
-          <form action="">
-          <div className="mb-6">
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-              <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required/>
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-              <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-        </div>
-      </div>
+    <main>
+      <Login/>
     </main>
   )
 }
